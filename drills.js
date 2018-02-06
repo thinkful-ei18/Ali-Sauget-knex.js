81e65a39a6a9e76adde0e471a7742005af8a6afe
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-02-06 11:00:37 -0500

    installed dependencies

--- drills.js
+++ drills.js
@@ -27,6 +27,14 @@ process.stdout.write('\x1Bc');
 //   .limit(6)
 //   .then(results => console.log(results));
 
+knex
+  .select('id', 'name')
+  .from('resturants')
+  .where({ cuisine: 'Italian' })
+  .limit(10)
+  .then(results => console.log(results));
+
+
 // Destroy the connection pool
 knex.destroy().then(() => {
   console.log('database connection closed');
